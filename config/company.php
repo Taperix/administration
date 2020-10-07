c044b23d0622899b54b6561208bc2bd417cafd0d
Lars Wiegers <larswiegers@live.nl>
2020-10-07 18:40:29 +0200

    company info

---
+++ config/company.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'Taperix',
+    'logo' => resource_path('images/taperix-trimmed.png'),
+    'address' => 'Hoge bergweg 16',
+    'city' => 'Beekbergen',
+    'zipcode' => '7361 GS',
+];
