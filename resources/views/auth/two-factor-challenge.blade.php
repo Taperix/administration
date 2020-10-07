055ab9e0983806cde7f424943ffc04b8d8780928
Lars Wiegers <larswiegers@live.nl>
2020-10-07 18:47:52 +0200

    change the logo

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="/images/taperix-logo-small.png" alt="logo">
         </x-slot>
 
         <div x-data="{ recovery: false }">
