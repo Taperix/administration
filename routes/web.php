e5ae31dc053ca8a463bd3b5f83bf92d0c5e7e81f
Lars Wiegers <larswiegers@live.nl>
2020-10-07 18:48:20 +0200

    added the invoice routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\InvoicePdfController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
+Route::resource('invoices', InvoiceController::class);
+Route::get('invoices/{invoice}/pdf', InvoicePdfController::class);
