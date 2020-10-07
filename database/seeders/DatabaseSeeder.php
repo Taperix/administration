afefc1a6214805e15f25e26d4ca3a19f11307f0d
Lars Wiegers <larswiegers@live.nl>
2020-10-07 18:50:16 +0200

    add in default user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $user = User::factory()->create([
+            'email' => 'testuser@taperix.com',
+            'password' => Hash::make('yO$27$*z9njy@OJYEWvylo7')
+        ]);
+        $team = Team::factory()->create([
+            'user_id' => $user->id,
+            'name' => 'testuser\'s Team',
+            'personal_team' => true
+        ]);
+        $this->call(InvoiceSeeder::class);
     }
 }
