20be344634e50a9873024fa7095b04f2e82198b1
Lars Wiegers <larswiegers@live.nl>
2020-10-08 16:22:02 +0200

    add the states to invoice and add recipient

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,17 +2,37 @@
 
 namespace App\Models;
 
+use App\States\Invoices\DraftState;
+use App\States\Invoices\InvoiceState;
+use App\States\Invoices\ReadyState;
+use App\States\Invoices\SentState;
 use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\ModelStates\HasStates;
 
 class Invoice extends Model
 {
-    use HasFactory;
+    use HasFactory, HasStates;
 
-    protected $fillable = ['title', 'is_draft', 'due_at'];
+    protected $fillable = ['title', 'due_at'];
+
+    protected $dates = ['due_at', 'sent_when', 'updated_at', 'created_at'];
+
+    protected function registerStates(): void
+    {
+        $this
+            ->addState('state', InvoiceState::class)
+            ->default(DraftState::class)
+            ->allowTransition(DraftState::class, ReadyState::class)
+            ->allowTransition(ReadyState::class, SentState::class);
+    }
 
     public function salesman() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function recipient() {
         return $this->belongsTo(User::class);
     }
 
