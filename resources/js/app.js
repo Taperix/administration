6926d82c8b51e2fe473852cd8b15c7df01114675
Lars Wiegers <larswiegers@live.nl>
2020-10-08 16:22:27 +0200

    use the sweetalert in vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,17 @@ import Vue from 'vue';
 
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
+import VueSweetalert2 from 'vue-sweetalert2';
+
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
 import PortalVue from 'portal-vue';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(VueSweetalert2);
+
 
 const app = document.getElementById('app');
 
