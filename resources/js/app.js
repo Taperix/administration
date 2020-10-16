ca17bd2c4f81d7cfa53972e40e5c99eb5cb92cf5
Lars Wiegers <larswiegers@live.nl>
2020-10-16 16:33:01 +0200

    add in the tab components

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,15 +9,25 @@ import VueSweetalert2 from 'vue-sweetalert2';
 // If you don't need the styles, do not connect
 import 'sweetalert2/dist/sweetalert2.min.css';
 import PortalVue from 'portal-vue';
+import {Tabs, Tab} from 'vue-tabs-component';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
 Vue.use(VueSweetalert2);
 
+Vue.component('tabs', Tabs);
+Vue.component('tab', Tab);
 
 const app = document.getElementById('app');
-
+Vue.mixin({
+    methods: {
+        ucfirst:(s) => {
+            if (typeof s !== 'string') return ''
+            return s.charAt(0).toUpperCase() + s.slice(1)
+        }
+    }
+});
 new Vue({
     render: (h) =>
         h(InertiaApp, {
