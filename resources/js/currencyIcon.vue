6027e943c7f0d89c231618ca70964f4c6013b730
Lars Wiegers <larswiegers@live.nl>
2020-10-16 16:06:01 +0200

    add in an component to show the right currency

---
+++ resources/js/currencyIcon.vue
@@ -0,0 +1,28 @@
+<template>
+    <span class=" text-gray-400">
+        {{icon}}
+    </span>
+</template>
+<script>
+    export default {
+        props: ['type'],
+        computed: {
+            icon: function() {
+                let icon = '';
+                switch(this.type) {
+                    case "EUR":
+                        icon = '€';
+                        break;
+                    case "USD":
+                        icon = "$";
+                        break;
+                    default:
+                        icon = "CURRENCY NOT SPECIFIED";
+                        console.warn('Currency not specified, please specify one (Examples : EUR, USD)');
+                }
+
+                return icon;
+            }
+        }
+    }
+</script>
